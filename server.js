25558820f0dbd856aa3524a11ec72c9effb69a85
Aleš Pros <alisek.pros@seznam.cz>
2017-01-30 13:36:50 +0100

    Checks if the expression is valid

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ var MongoClient = mongo.MongoClient
 var url = 'mongodb://localhost:27017';
 // Use connect method to connect to the Server 
 
+var expression = /^(https?:\/\/)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+var regex = new RegExp(expression);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/server.html")
@@ -22,23 +24,28 @@ app.get('/', function (req, res) {
 app.get("/new/*", function(req, res) {
     var site = req.params[0]
 
-    MongoClient.connect(url, function(err, db) {
-        assert.equal(null, err);
-        console.log("Connected correctly to server");
+    if (!site.match(regex)) {
+        res.end("expression is invalid")
+    } else {
+        MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            console.log("Connected correctly to server");
 
-        var collection = db.collection('urlshortener')
-        Promise.resolve(collection.updateOne({"site":site}, {"site":site}, {upsert:true}, function(err, result) {}))
-            .then(collection.findOne({"site":site}, function(err, result) {
-                var odpoved = {
-                    "original_url":site,
-                    "short_url":"http://nodeweb.local/" + result._id
-                }
-                res.send(odpoved)
-                console.log(result)
-            }))
-        //collection.find().toArray(function(err, result) {console.log(result)} )
-        db.close();
-    });
+            var collection = db.collection('urlshortener')
+            Promise.resolve(collection.updateOne({"site":site}, {"site":site}, {upsert:true}, function(err, result) {}))
+                .then(collection.findOne({"site":site}, function(err, result) {
+                    var odpoved = {
+                        "original_url":site,
+                        "short_url":"http://nodeweb.local/" + result._id
+                    }
+                    res.send(odpoved)
+                }))
+            //collection.find().toArray(function(err, result) {console.log(result)} )
+            db.close();
+        });
+    }
+
+
 })
 
 app.get("/*", function (req, res) {
@@ -50,12 +57,11 @@ app.get("/*", function (req, res) {
 
         var collection = db.collection('urlshortener')
         collection.findOne({"_id":id}, function(err, result) {
-            console.log(result.site)
             res.redirect(result.site)
         })
     })
 })
 
 app.listen(3000, function(){
-    console.log("spusteno")
+    console.log("running")
 })
