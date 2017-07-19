1eff90efb10ba0d839338bb85ac249d16b58d86c
Aleš Pros <alisek.pros@seznam.cz>
2017-07-19 11:37:49 +0200

    Database assigned at the start to the variable

--- server.js
+++ server.js
@@ -15,6 +15,19 @@ var url = 'mongodb://localhost:27017';
 var expression = /^(https?:\/\/)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
 var regex = new RegExp(expression);
 
+
+var database
+var collection
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+
+    //asign the database to a variable so we can close it when needed -> database.close()
+    database = db
+
+    collection = db.collection('urlshortener')
+})
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/server.html")
   
@@ -26,24 +39,16 @@ app.get("/new/*", function(req, res) {
     if (!site.match(regex)) {
         res.end("expression is invalid")
     } else {
-        MongoClient.connect(url, function(err, db) {
-            assert.equal(null, err);
-            console.log("Connected correctly to server");
-
-            var collection = db.collection('urlshortener')
-            Promise.resolve(collection.updateOne({"site":site}, {"site":site}, {upsert:true}, function(err, result) {}))
-                .then(collection.findOne({"site":site}, function(err, result) {
-                    var odpoved = {
-                        "original_url":site,
-                        "short_url":"http://nodeweb.local/" + result._id
-                    }
-                    res.send(odpoved)
-                }))
-            //collection.find().toArray(function(err, result) {console.log(result)} )
-            db.close();
-        });
+        Promise.resolve(collection.updateOne({"site":site}, {"site":site}, {upsert:true}, function(err, result) {}))
+            .then(collection.findOne({"site":site}, function(err, result) {
+                var odpoved = {
+                    "original_url":site,
+                    "short_url":"http://nodeweb.local/" + result._id
+                }
+                res.send(odpoved)
+            }))
+        collection.find().toArray(function(err, result) {console.log(result)} )
     }
-
 })
 
 app.get('/favicon.ico', function(req, res) {
@@ -52,15 +57,8 @@ app.get('/favicon.ico', function(req, res) {
 
 app.get("/*", function (req, res) {
     var id = new mongo.ObjectID(req.params[0])
-
-    MongoClient.connect(url, function (err, db) {
-        assert.equal(null, err);
-        console.log("Connected correctly to server");
-
-        var collection = db.collection('urlshortener')
-        collection.findOne({"_id":id}, function(err, result) {
+    collection.findOne({"_id":id}, function(err, result) {
             res.redirect(result.site)
-        })
     })
 })
 
