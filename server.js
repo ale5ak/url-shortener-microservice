056cbd765e085945c7aba7f60a91b23705f2d3fe
Aleš Pros <alisek.pros@seznam.cz>
2017-07-19 11:39:28 +0200

    Reformat code

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ Promise.promisifyAll(require("mongodb"));
 
 const mongo = require('mongodb')
 var MongoClient = mongo.MongoClient
-  , assert = require('assert');
- 
+    , assert = require('assert');
+
 // Connection URL
 var url = 'mongodb://localhost:27017';
 // Use connect method to connect to the Server 
@@ -18,7 +18,7 @@ var regex = new RegExp(expression);
 
 var database
 var collection
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function (err, db) {
     assert.equal(null, err);
     console.log("Connected correctly to server");
 
@@ -29,39 +29,39 @@ MongoClient.connect(url, function(err, db) {
 })
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/server.html")
-  
+    res.sendFile(__dirname + "/server.html")
 })
 
-app.get("/new/*", function(req, res) {
+app.get("/new/*", function (req, res) {
     var site = req.params[0]
 
     if (!site.match(regex)) {
         res.end("expression is invalid")
     } else {
-        Promise.resolve(collection.updateOne({"site":site}, {"site":site}, {upsert:true}, function(err, result) {}))
-            .then(collection.findOne({"site":site}, function(err, result) {
+        Promise.resolve(collection.updateOne({"site": site}, {"site": site}, {upsert: true}, function (err, result) {
+        }))
+            .then(collection.findOne({"site": site}, function (err, result) {
                 var odpoved = {
-                    "original_url":site,
-                    "short_url":"http://nodeweb.local/" + result._id
+                    "original_url": site,
+                    "short_url": "http://nodeweb.local/" + result._id
                 }
                 res.send(odpoved)
             }))
-        collection.find().toArray(function(err, result) {console.log(result)} )
+        //collection.find().toArray(function (err, result) { console.log(result) })
     }
 })
 
-app.get('/favicon.ico', function(req, res) {
+app.get('/favicon.ico', function (req, res) {
     res.status(204);
 });
 
 app.get("/*", function (req, res) {
     var id = new mongo.ObjectID(req.params[0])
-    collection.findOne({"_id":id}, function(err, result) {
-            res.redirect(result.site)
+    collection.findOne({"_id": id}, function (err, result) {
+        res.redirect(result.site)
     })
 })
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("running")
 })
